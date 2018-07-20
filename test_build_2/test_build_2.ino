22bbf774a8fb6039b5fc2771dd1fc5b1d63a24ff
Andrew Kramer <1988kramer@gmail.com>
2018-07-20 16:11:58 -0600

    added functions to set exposure time and run and reset motor

--- test_build_2/test_build_2.ino
+++ test_build_2/test_build_2.ino
@@ -29,6 +29,16 @@
 // GPS module constants
 #define GPS_ENABLE_PIN 19
 
+// SD card chip select pin
+#define SD_CS_PIN 29
+
+// motor control pins
+#define LIMIT_SWITCH_PIN 20
+#define MOTOR_ENC_A      2
+#define MOTOR_ENC_B      1
+#define MOTOR_PWM        3
+#define MOTOR_DIR_PIN    0
+
 
 /* - - - - - - - - - encoder variables - - - - - - - - - - */
 
@@ -78,6 +88,25 @@ int latitude_, longitude_;
 float declination_;
 
 
+/* - - - - - - - - - Motor Control Variables - - - - - - - */
+
+int exposure_time_ = 0;
+volatile long motor_count_;
+long new_motor_count_, old_motor_count_;
+const double kP_ = 0.018;
+const double kI_ = 0.00005;
+const double kD_ = 0.0001;
+
+unsigned long last_pid_update_time_;
+double last_pid_speed_;
+double last_speed_;
+double set_point_;
+int pwm_setting_;
+double i_term_;
+const int pid_delta_t_target_ = 250;
+unsigned long motor_start_time_;
+
+
 /* - - - - - - - - State Control Structure - - - - - - - - */
 
 /* The program's state and behavior are managed through
@@ -120,6 +149,13 @@ void setup()
   DEBUG_PORT.println("initializing states");
   cur_state_ = initStatePointers();
   DEBUG_PORT.println("states initialized");
+  
+  attachInterrupt(2, readMotorEncoder, CHANGE);
+  pinMode(MOTOR_DIR_PIN, OUTPUT);
+  pinMode(LIMIT_SWITCH_PIN, INPUT_PULLUP);
+  pinMode(MOTOR_ENC_A, INPUT);
+  pinMode(MOTOR_ENC_B, INPUT);
+  pinMode(MOTOR_PWM, OUTPUT);
 }
 
 void loop() 
@@ -307,91 +343,6 @@ void orientFilter()
   }
 }
 
-stateNode* initStatePointers()
-{
-  stateNode *orient_node = new stateNode();
-  strcpy(orient_node->disp_options[0],"1) auto orient");
-  strcpy(orient_node->disp_options[1],"2) manual orient");
-  strcpy(orient_node->disp_options[2],"3) help");
-  orient_node->num_lines = 3;
-  orient_node->num_options = 3;
-  orient_node->action = selectFromList;
-  orient_node->init_func = NULL;
-  orient_node->end_func = NULL;
-  orient_node->switch_active = true;
-
-  stateNode *gps_node = new stateNode();
-  stateNode *dec_lookup = new stateNode();
-  stateNode *auto_orient = new stateNode();
-  stateNode *manual_orient = new stateNode();
-  stateNode *orient_help = new stateNode();
-
-  orient_node->next_states[0] = gps_node;
-  orient_node->next_states[1] = manual_orient;
-  orient_node->next_states[2] = orient_help;
-
-  gps_node->next_states[0] = dec_lookup;
-  gps_node->num_options = 1;
-  gps_node->num_lines = 2;
-  strcpy(gps_node->disp_options[0],"finding latitude");
-  strcpy(gps_node->disp_options[1],"and longitude.");
-  gps_node->init_func = initGPS; 
-  gps_node->action = updateGPS; // not yet implemented
-  gps_node->end_func = endGPS; // not yet implemented
-  gps_node->switch_active = false;
-
-  dec_lookup->next_states[0] = auto_orient;
-  dec_lookup->num_options = 1;
-  dec_lookup->num_lines = 2;
-  strcpy(dec_lookup->disp_options[0],"finding local");
-  strcpy(dec_lookup->disp_options[1],"magnetic declination");
-  dec_lookup->init_func = initSD; // not yet implemented
-  dec_lookup->action = lookupDeclination; // not yet implemented
-  dec_lookup->end_func = endSD; // not yet implemented
-  dec_lookup->switch_active = false;
-
-  stateNode *dead_end = new stateNode();
-  strcpy(dead_end->disp_options[0],"nothing here");
-  dead_end->num_options = 1;
-  dead_end->num_lines = 1;
-  dead_end->action = displayText;
-  dead_end->init_func = NULL;
-  dead_end->end_func = NULL;
-  dead_end->next_states[0] = dead_end;
-
-  auto_orient->num_options = 1;
-  auto_orient->num_lines = 1;
-
-  strcpy(auto_orient->disp_options[0], "press OK when done");
-  auto_orient->init_func = initIMU;
-  auto_orient->action = orientFilter;
-  auto_orient->end_func = NULL;
-  auto_orient->next_states[0] = dead_end;
-  auto_orient->switch_active = true;
-  
-  strcpy(manual_orient->disp_options[0], "align the hinge axis");
-  strcpy(manual_orient->disp_options[1], "with the pole star");
-  strcpy(manual_orient->disp_options[2], "and press OK");
-  manual_orient->num_options = 1;
-  manual_orient->num_lines = 3;
-  manual_orient->action = displayText;
-  manual_orient->init_func = NULL;
-  manual_orient->end_func = NULL;
-  manual_orient->next_states[0] = dead_end;
-  manual_orient->switch_active = true;
-
-  strcpy(orient_help->disp_options[0], "orient help stub");
-  orient_help->num_options = 1;
-  orient_help->num_lines = 1;
-  orient_help->action = displayText;
-  orient_help->init_func = NULL;
-  orient_help->end_func = NULL;
-  orient_help->next_states[0] = orient_node;
-  orient_help->switch_active = true;
-
-  return orient_node;
-}
-
 void initDisplay()
 {
   display_.begin();
@@ -479,19 +430,115 @@ static void printGPSStatus(const gps_fix &fix)
   }
 }
 
-void initSD()
+void strAppend(char *str1, uint8_t size1, char *str2, uint8_t size2)
 {
-  
+  uint8_t index1 = 0;
+  uint8_t index2 = 0;
+  while (index1 < size1 && str1[index1] != '\0')
+  {
+    index1++;
+  }
+  while (index1 < size1 && index2 < size2 && str2[index2] != '\0')
+  {
+    str1[index1++] = str2[index2++];
+  }
 }
 
-void endSD()
+void getFileName(int8_t lat, int8_t lon, int year, char *filename, uint8_t name_size)
 {
+  uint8_t str_size = 10;
+  memset(filename, '\0', name_size);
   
+  char year_str[str_size];
+  memset(year_str, '\0', str_size);
+  itoa(year, year_str, 10);
+  strAppend(filename, name_size, year_str, str_size);
+  strAppend(filename, name_size, "_0/", 3);
+
+  char lat_str[str_size];
+  memset(lat_str, '\0', str_size);
+  itoa(abs(lat), lat_str, 10);
+
+  if (lat < 0)
+    strAppend(filename, name_size, "n", 1);
+  strAppend(filename, name_size, lat_str, str_size);
+
+  strAppend(filename, name_size, ".csv", 4);
+}
+
+void readLine(char *buf, File dec_file)
+{
+  int index = 0;
+  do 
+  {
+    buf[index++] = dec_file.read();
+  } while (dec_file.peek() != '\n');
+}
+
+void initSD()
+{
+  if (!SD.begin(SD_CS_PIN))
+    DEBUG_PORT.println("SD card initialization failed");
+  DEBUG_PORT.println("SD card initialized");
 }
 
 void lookupDeclination()
 {
-  
+  // hardcoding latitude and longitude until GPS is working
+  latitude_ = 40;
+  longitude_ = -105;
+  int year = 2018;
+  char filename[40];
+  memset(filename, '\0', 40);
+  getFileName(latitude_, longitude_, year, filename, 40);
+
+  File dec_file;
+  dec_file = SD.open(filename, FILE_READ);
+
+  if (dec_file)
+  {
+    DEBUG_PORT.print("file ");
+    DEBUG_PORT.print(filename);
+    DEBUG_PORT.println(" opened successfully");
+    bool target_found = false;
+    while (dec_file.available() && !target_found)
+    {
+      char line[25];
+      memset(line, '\0', 25);
+      readLine(line, dec_file);
+      char *token = strtok(line, ",");
+      int8_t this_lat = atoi(token);
+      token = strtok(NULL, ",");
+      int8_t this_lon = atoi(token);
+      token = strtok(NULL, ",");
+      declination_ = atof(token);
+
+      target_found = (this_lat == latitude_ && 
+                      this_lon == longitude_);
+    }
+    advance_state_ = true;
+    DEBUG_PORT.println("found declination");
+  }
+  DEBUG_PORT.println("unable to find declination");
+}
+
+void setExposure()
+{
+  if (disp_state_change_ || encoder_pos_ != last_encoder_pos_)
+  {
+    if (encoder_pos_ < 0)
+    {
+      encoder_pos_ = 0;
+      last_encoder_pos_ = 0;
+    }
+    exposure_time_ = encoder_pos_;
+    display_.clearDisplay();
+    display_.setCursor(0,0);
+    display_.println("Select exposure");
+    display_.println(exposure_time_);
+    printText();
+    display_.display();
+  }
 }
 
 void encoderAUpdate(uint8_t enc_a_state, uint8_t enc_b_state)
@@ -558,10 +605,18 @@ void printOrientation()
 {
   display_.print("roll: ");
   display_.println(roll_);
+  display_.println("target: 0.0");
+
+  int8_t target_pitch = -1 * latitude_;
   display_.print("pitch: ");
   display_.println(pitch_);
+  display_.print("target: ");
+  display_.println(target_pitch);
+  
   display_.print("yaw: ");
   display_.println(yaw_);
+  display_.print("target: ");
+  display_.println(declination_);
   display_.println();
 
   last_filter_pub_ += filter_pub_time_;
@@ -591,25 +646,231 @@ void convertRawMag(float &x, float &y, float &z)
   z = (float)mag_.m.z / mag_scale_;
 }
 
-void strAppend(char *str1, uint8_t size1, char *str2, uint8_t size2)
+void updatePID()
 {
-  uint8_t index1 = 0;
-  uint8_t index2 = 0;
-  while (index1 < size1 && str1[index1] != '\0')
+  unsigned long cur_time = millis();
+  if (cur_time - last_pid_update_time_ >= pid_delta_t_target_)
   {
-    index1++;
-  }
-  while (index1 < size1 && index2 < size2 && str2[index2] != '\0')
-  {
-    str1[index1++] = str2[index2++];
+    double deltaT = (double)(cur_time - last_pid_update_time_) / 1000.0;
+    double cur_speed = getSpeed(deltaT);
+    updatePWM(cur_speed);
+    analogWrite(MOTOR_PWM, pwm_setting_);
+    last_pid_update_time_ = cur_time;
   }
 }
 
-void readLine(char *buf, File dec_file)
+double getSpeed(double deltaT)
 {
-  int index = 0;
-  do 
+  old_motor_count_ = new_motor_count_;
+  new_motor_count_ = motor_count_;
+  int difference = new_motor_count_ - old_motor_count_;
+  if (difference < 0) difference *= -1;
+  double cur_speed;
+  if (difference < 50000)
   {
-    buf[index++] = dec_file.read();
-  } while (dec_file.peek() != '\n');
+    cur_speed = (double)difference / deltaT;
+    last_speed_ = cur_speed;
+  }
+  else
+  {
+    cur_speed = last_speed_;
+  }
+  return cur_speed;
 }
+
+void updatePWM(double cur_speed)
+{
+  double error = set_point_ - cur_speed;
+  i_term_ += kI_ * error;
+  double d_input = cur_speed - last_pid_speed_;
+  int adjustment = (kP_ * error) + i_term_ - (kD_ * d_input);
+  pwm_setting_ += adjustment;
+  if (pwm_setting_ > 255) pwm_setting_ = 255;
+  else if (pwm_setting_ < 0) pwm_setting_ = 0;
+  last_pid_speed_ = cur_speed;
+}
+
+void resetPIDVars()
+{
+  i_term_ = 0.0;
+  last_speed_ = 0.0;
+  last_pid_speed_ = 0.0;
+  motor_count_ = 0;
+  new_motor_count_ = 0;
+  old_motor_count_ = 0;
+  last_pid_update_time_ = millis();
+}
+void initMotor()
+{
+  digitalWrite(MOTOR_DIR_PIN, HIGH);
+  set_point_ = 98.0;
+  resetPIDVars();
+  
+  motor_start_time_ = millis();
+  display_.clearDisplay();
+  display_.display();
+}
+
+void runMotor()
+{
+  while (millis() - motor_start_time_ < exposure_time_ * 1000)
+    updatePID();
+  advance_state_ = true;
+}
+
+void stopMotor()
+{
+  analogWrite(MOTOR_PWM, 0);
+}
+
+void resetInit()
+{
+  digitalWrite(MOTOR_DIR_PIN, LOW);
+  set_point_ = 800.0;
+  resetPIDVars();
+}
+
+void resetMotor()
+{
+  while(digitalRead(LIMIT_SWITCH_PIN) == HIGH)
+  {
+    updatePID();
+    displayText();
+  }
+  advance_state_ = true;
+}
+
+void readMotorEncoder()
+{
+  if (digitalRead(MOTOR_ENC_A) == HIGH)
+  {
+    if (digitalRead(MOTOR_ENC_B) == LOW)
+      motor_count_++;
+    else
+      motor_count_--;
+  }
+  else
+  {
+    if (digitalRead(MOTOR_ENC_B) == LOW)
+      motor_count_--;
+    else
+      motor_count_++;
+  }
+}
+
+stateNode* initStatePointers()
+{
+  stateNode *orient_node = new stateNode();
+  strcpy(orient_node->disp_options[0],"1) auto orient");
+  strcpy(orient_node->disp_options[1],"2) manual orient");
+  strcpy(orient_node->disp_options[2],"3) help");
+  orient_node->num_lines = 3;
+  orient_node->num_options = 3;
+  orient_node->action = selectFromList;
+  orient_node->init_func = NULL;
+  orient_node->end_func = NULL;
+  orient_node->switch_active = true;
+
+  stateNode *gps_node = new stateNode();
+  stateNode *dec_lookup = new stateNode();
+  stateNode *auto_orient = new stateNode();
+  stateNode *manual_orient = new stateNode();
+  stateNode *orient_help = new stateNode();
+  stateNode *set_exposure = new stateNode();
+  stateNode *run_motor = new stateNode();
+  stateNode *reset_motor = new stateNode();
+
+  //orient_node->next_states[0] = gps_node;
+  orient_node->next_states[0] = dec_lookup;
+  orient_node->next_states[1] = manual_orient;
+  orient_node->next_states[2] = orient_help;
+
+  gps_node->next_states[0] = dec_lookup;
+  gps_node->num_options = 1;
+  gps_node->num_lines = 2;
+  strcpy(gps_node->disp_options[0],"finding latitude");
+  strcpy(gps_node->disp_options[1],"and longitude.");
+  gps_node->init_func = initGPS; 
+  gps_node->action = updateGPS; // not yet implemented
+  gps_node->end_func = endGPS; // not yet implemented
+  gps_node->switch_active = false;
+
+  dec_lookup->next_states[0] = auto_orient;
+  dec_lookup->num_options = 1;
+  dec_lookup->num_lines = 2;
+  strcpy(dec_lookup->disp_options[0],"finding local");
+  strcpy(dec_lookup->disp_options[1],"magnetic declination");
+  dec_lookup->init_func = initSD; 
+  dec_lookup->action = lookupDeclination;
+  dec_lookup->end_func = NULL;
+  dec_lookup->switch_active = false;
+
+  stateNode *dead_end = new stateNode();
+  strcpy(dead_end->disp_options[0],"nothing here");
+  dead_end->num_options = 1;
+  dead_end->num_lines = 1;
+  dead_end->action = displayText;
+  dead_end->init_func = NULL;
+  dead_end->end_func = NULL;
+  dead_end->next_states[0] = dead_end;
+
+  auto_orient->num_options = 1;
+  auto_orient->num_lines = 3;
+
+  strcpy(auto_orient->disp_options[0], "press OK when done");
+  auto_orient->init_func = initIMU;
+  auto_orient->action = orientFilter;
+  auto_orient->end_func = NULL;
+  auto_orient->next_states[0] = set_exposure;
+  auto_orient->switch_active = true;
+  
+  strcpy(manual_orient->disp_options[0], "align the hinge axis");
+  strcpy(manual_orient->disp_options[1], "with the pole star");
+  strcpy(manual_orient->disp_options[2], "and press OK");
+  manual_orient->num_options = 1;
+  manual_orient->num_lines = 3;
+  manual_orient->action = displayText;
+  manual_orient->init_func = NULL;
+  manual_orient->end_func = NULL;
+  manual_orient->next_states[0] = set_exposure;
+  manual_orient->switch_active = true;
+
+  strcpy(set_exposure->disp_options[0], "press OK to");
+  strcpy(set_exposure->disp_options[1], "start exposure");
+  set_exposure->num_options = 1;
+  set_exposure->num_lines = 2;
+  set_exposure->init_func = NULL;
+  set_exposure->action = setExposure; // not implemented
+  set_exposure->end_func = NULL;
+  set_exposure->next_states[0] = run_motor;
+  set_exposure->switch_active = true;
+
+  run_motor->num_options = 1;
+  run_motor->num_lines = 0;
+  run_motor->init_func = initMotor; // not implemented
+  run_motor->action = runMotor; // not implemented
+  run_motor->end_func = stopMotor;
+  run_motor->next_states[0] = reset_motor;
+  run_motor->switch_active = false;
+
+  strcpy(reset_motor->disp_options[0], "resetting motor");
+  reset_motor->num_options = 1;
+  reset_motor->num_lines = 1;
+  reset_motor->init_func = resetInit; // not implemented
+  reset_motor->action = resetMotor; // not implemented
+  reset_motor->end_func = stopMotor;
+  reset_motor->next_states[0] = set_exposure;
+  reset_motor->switch_active = false;
+
+  strcpy(orient_help->disp_options[0], "orient help stub");
+  orient_help->num_options = 1;
+  orient_help->num_lines = 1;
+  orient_help->action = displayText;
+  orient_help->init_func = NULL;
+  orient_help->end_func = NULL;
+  orient_help->next_states[0] = orient_node;
+  orient_help->switch_active = true;
+
+  return orient_node;
+}
+
